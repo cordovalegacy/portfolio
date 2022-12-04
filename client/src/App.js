87ed6f88549c397fd790c240e84ac76643c101eb
Brendan Cordova <cordovalegacy19@gmail.com>
2022-12-03 17:54:39 -0800

    Starting Front End

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav>
+          <Link to="/portfolio/a">Technologies</Link>
+          <Link to="/portfolio/b">Experience</Link>
+          <Link to="/">Picture</Link>
+          <Link to="/portfolio/d">Personality</Link>
+          <Link to="/portfolio/e">Contact Me</Link>
+        </nav>
+        <Routes>
+          <Route />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
