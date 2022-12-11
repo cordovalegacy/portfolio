24facb7d8d64c1ae697e0fd01913161b190beb6e
Brendan Cordova <cordovalegacy19@gmail.com>
2022-12-10 17:28:04 -0800

    Resume/Contact Form Functionality Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,118 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
 import Home from './components/home';
+import emailjs from '@emailjs/browser';
+import { useState } from 'react';
 
 function App() {
 
+  const [fullName, setFullName] = useState("");
+  const [company, setCompany] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [message, setMessage] = useState("");
+
   const getResume = () => {
     console.log('Downloading Resume');
+    document.getElementById("resume-alert").style.display = "flex"
+  }
+
+  const closeAlert = () => {
+    document.getElementById("resume-alert").style.display = "none"
+  }
+
+  const showSources = (e) => {
+    document.getElementById("footer-sources").style.display = "block"
+  }
+
+  const hideSources = (e) => {
+    document.getElementById("footer-sources").style.display = "none"
+  }
+
+  const openContactMeForm = (e) => {
+    document.getElementById("contact-me-alert-form").style.display = "flex"
+    document.getElementById("resume-alert-top").style.display = "none"
+  }
+
+  const closeForm = (e) => {
+    document.getElementById("contact-me-alert-form").style.display = "none"
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const contactForm = {
+      fullName,
+      company,
+      emailAddress,
+      message
+    }
+    emailjs.send('service_id', 'Portfolio', contactForm, 'LW4RMYIvhRvf0Fz9c')
+      .then((res) => {
+        closeForm();
+        console.log("Success!", res.data);
+      }, (err) => {
+        console.log("Email was not sent", err);
+      });
   }
 
   return (
     <BrowserRouter>
-      <div className="App">
-        <nav className='left-nav'>
-          <Link to="/portfolio/technologies">Technologies</Link>
-          <Link to="/portfolio/experience">Experience</Link>
-          <Link to="/">Brendan</Link>
-          <Link to="/portfolio/personality">Personality</Link>
-          <Link to="/portfolio/contact">Contact Me</Link>
-        </nav>
-        <nav className='top-nav'>
-          <a href='https://github.com/cordovalegacy?tab=repositories'>Github</a>
-          <a href='https://www.linkedin.com/in/brendan-cordova-2874011ba/'>LinkedIn</a>
-          <a href='resume.png' id='download-button' download>Resume</a>
-          <a href='https://www.instagram.com/legacybuildspc/?next=%2F'>Instagram</a>
-          <a href='http://35.164.101.114/'>Legacy Builds</a>
-        </nav>
-        <div className='nav-content-spacer'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-          </Routes>
+      <div className='App-container'>
+        <div className="App">
+          <nav className='left-nav'>
+            <Link to="/portfolio/technologies">Technologies</Link>
+            <Link to="/portfolio/experience">Experience</Link>
+            <Link to="/">Brendan</Link>
+            <Link to="/portfolio/personality">Personality</Link>
+            <Link to="/portfolio/contact">Contact Me</Link>
+          </nav>
+          <nav className='top-nav'>
+            <a href='https://github.com/cordovalegacy?tab=repositories'>Github</a>
+            <a href='https://www.linkedin.com/in/brendan-cordova-2874011ba/'>LinkedIn</a>
+            <a href='resume.png' id='download-button' onClick={getResume} download>Resume</a>
+            <a href='https://www.instagram.com/legacybuildspc/?next=%2F'>Instagram</a>
+            <a href='http://35.164.101.114/'>Legacy Builds</a>
+          </nav>
+          <div id='resume-alert'>
+            <div id='resume-alert-top'>
+              <h2>Thank you for viewing my resume!</h2>
+              <h3>Please reach out to me if you have any questions.</h3>
+              <div id='alert-button-row'>
+                <button id='alert-button' onClick={closeAlert}>Continue</button>
+                <button id='alert-button' onClick={openContactMeForm}>Contact Me</button>
+              </div>
+            </div>
+            <form id='contact-me-alert-form' onSubmit={submitHandler}>
+              <div className='contact-form-top-row'>
+                <h2 style={{ textDecoration: "underline" }}>Contact Form</h2>
+                <button id='close-form' onClick={closeForm}>X</button>
+              </div>
+              <label>Name:</label>
+              <input type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} />
+              <label>Company:</label>
+              <input type="text" value={company} onChange={(e) => setCompany(e.target.value)} />
+              <label>Email Address:</label>
+              <input type="text" value={emailAddress} onChange={(e) => setEmailAddress(e.target.value)} />
+              <label>Message:</label>
+              <textarea type="text" value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+              <hr />
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+          <div className='nav-content-spacer'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+            </Routes>
+          </div>
         </div>
+        <footer onMouseOver={showSources} onMouseOut={hideSources}>
+          Website by Brendan Cordova
+          <div id='footer-sources'>
+            <p>Source 1</p>
+            <p>Source 2</p>
+            <p>Source 3</p>
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Home_Photo from '../img/home_photo.jpg';
-import Ninja from '../img/down1.png';
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
@@ -23,86 +22,57 @@ const Home = () => {
         setIsOpenTech(!isOpenTech);
     }
 
-    var leftValue = 400, topValue = 500;
-    function update() {
-        document.getElementById("character").style.left = leftValue + "px";
-        document.getElementById("character").style.top = topValue + "px";
-    }
-    document.onkeydown = function (e) {
-        console.log('e: ', e);
-        console.log('e.key: ', e.key);
-        if (e.key == 65 && leftValue > 100) { // LEFT
-            leftValue = leftValue - 10;
-        }
-        else if (e.key == 68 && leftValue < 400) { // RIGHT
-            leftValue = leftValue + 10;
-        }
-        else if (e.key == 83 && topValue > 100) { // DOWN
-            topValue = topValue + 10;
-        }
-        else if (e.key == 87 && topValue < 500){ //UP
-            topValue = topValue - 10;
-        }
-
-        update();
-    }
-
     return (
-        <div className="home-container">
-            <div className="home-header-wrapper">
-                <h1 id="home-header">Brendan<span id="red-text"> Cordova</span></h1>
-                <div className="home-sub-header-wrapper">
-                    <h2 onClick={handleOpenDev} id="software-header">Software Developer</h2>
-                    <h2>||</h2>
-                    <h2 onClick={handleOpenTech} id="technician-header">Computer Technician</h2>
+            <div className="home-container">
+                <div className="home-header-wrapper">
+                    <h1 id="home-header">Brendan<span id="red-text"> Cordova</span></h1>
+                    <div className="home-sub-header-wrapper">
+                        <h2 onClick={handleOpenDev} id="software-header">Coding Dojo</h2>
+                        <h2>||</h2>
+                        <h2 onClick={handleOpenTech} id="technician-header">Legacy Builds, LLC</h2>
+                    </div>
+                    {!isOpenDev && !isOpenTech ? (
+                        <div className="home-default-content">
+                            <div id="dev-ninja">
+                                <h3>Designing</h3>
+                                <h3>Debugging</h3>
+                                <h3>Teaching</h3>
+                                <h3>Testing</h3>
+                                <h3>Innovating</h3>
+                                <h3>Security</h3>
+                                <h3>Solutions</h3>
+                                <h3>Communicating</h3>
+                                <h3>Creating</h3>
+                            </div>
+                            <div id="tech-ninja">
+                                <h3>Troubleshooting</h3>
+                                <h3>Technician</h3>
+                                <h3>Consulting</h3>
+                                <h3>Crafting</h3>
+                                <h3>Relations</h3>
+                                <h3>Sales</h3>
+                                <h3>Software</h3>
+                                <h3>Hardware</h3>
+                                <h3>Optimization</h3>
+                            </div>
+                        </div>
+                    ) : null}
+                    {isOpenDev ? (
+                        <div className="home-sub-header-wrapper">
+                            <h2 id="dev-sub-header">Software Developer - Teacher's Assistant</h2>
+                        </div>
+                    ) : null}
+                    {isOpenTech ? (
+                        <div className="home-sub-header-wrapper">
+                            <h2 id="tech-sub-header">Computer Technician - Full Stack Developer</h2>
+                        </div>
+                    ) : null}
                 </div>
-                {!isOpenDev && !isOpenTech ? (
-                    <div className="home-default-content">
-                        <div id="dev-ninja">
-                            <h3>Designing</h3>
-                            <h3>Debugging</h3>
-                            <h3>Teaching</h3>
-                            <h3>Testing</h3>
-                            <h3>Innovating</h3>
-                            <h3>Security</h3>
-                            <h3>Solutions</h3>
-                            <h3>Communicating</h3>
-                            <h3>Creating</h3>
-                        </div>
-                        <div id="character"></div>
-                        <div id="tech-ninja">
-                            <h3>Troubleshooting</h3>
-                            <h3>Technician</h3>
-                            <h3>Consulting</h3>
-                            <h3>Crafting</h3>
-                            <h3>Relations</h3>
-                            <h3>Sales</h3>
-                            <h3>Software</h3>
-                            <h3>Hardware</h3>
-                            <h3>Optimization</h3>
-                        </div>
-                    </div>
-                ) : null}
-                {isOpenDev ? (
-                    <div className="home-sub-header-wrapper">
-                        <h2 id="software-header">Software Developer</h2>
-                        <h2>Dev</h2>
-                        <h2 id="technician-header">Computer Technician</h2>
-                    </div>
-                ) : null}
-                {isOpenTech ? (
-                    <div className="home-sub-header-wrapper">
-                        <h2 id="software-header">Software Developer</h2>
-                        <h2>Tech</h2>
-                        <h2 id="technician-header">Computer Technician</h2>
-                    </div>
-                ) : null}
+                <div className="home-content-parent">
+                    <img id="home-photo" src={Home_Photo} alt="Home Page Photo" />
+                    <button id="home-button" onClick={() => { navigate('/technologies') }}>Get to know me!</button>
+                </div>
             </div>
-            <div className="home-content-parent">
-                <img id="home-photo" src={Home_Photo} alt="Home Page Photo" />
-                <button id="home-button" onClick={() => { navigate('/technologies') }}>Get to know me!</button>
-            </div>
-        </div>
     )
 }
 
